10c55e25dd448e0f37a06e6aff08ddf74b88d558
Ant722 <Ant722@mail.ru>
2023-05-04 16:01:23 +0300

    init commit

---
+++ src/main/java/org/example/Basket.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Map;
+
+import static org.example.Main.products;
+
+public class Basket {
+    protected Purchase[] purchases = new Purchase[products.size()];
+
+    public Basket(){
+    }
+    public void addPurchase(String title, int count) {
+        for (int i = 0; i < purchases.length; i++) {
+            if (purchases[i] == null) {
+                purchases[i] = new Purchase(title, count);
+                return;
+            }
+            if (purchases[i].title.equals(title)) {
+                purchases[i].count += count;
+                return;
+            }
+        }
+    }
+    public long sum(Map<String, Integer> prices) {
+        long sum = 0;
+        System.out.println("КОРЗИНА:");
+        for (int i = 0; i < purchases.length; i++) {
+            Purchase purchase = purchases[i];
+            if (purchase == null) continue;
+            System.out.println("\t" + purchase.title + " " + purchase.count + " шт. в сумме " + (purchase.count * prices.get(purchase.title)) + " руб.");
+            sum += purchase.count * prices.get(purchase.title);
+        }
+        return sum;
+    }
+}
